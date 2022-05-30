4e91c14cb203c6c579be2d338cc6e0519fbf4c8d
rupok <nurealamsiddiky@gmail.com>
2022-05-30 07:08:03 +0600

    firebase config changed

--- admin.js
+++ admin.js
@@ -4,7 +4,10 @@ admin.initializeApp({
     credential: admin.credential.cert({
         type: process.env.FIREBASE_ADMIN_TYPE,
         project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+        private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(
+            /\\n/g,
+            "\n"
+        ),
         private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
         client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
